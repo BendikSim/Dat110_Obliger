52984cbd1a93e98464fb0c90b40e50c5324b20f2
Bendik <Bendiksim98@gmail.com>
2020-04-28 14:38:28 +0200

    Task 2 og 3✔

---
+++ Prosjekt4/dat110-project4-startcode-cloudservice/ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/AccessLog.java
@@ -0,0 +1,54 @@
+package no.hvl.dat110.ac.restservice;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.google.gson.Gson;
+import com.google.gson.internal.$Gson$Preconditions;
+
+public class AccessLog {
+	
+	// atomic integer used to obtain identifiers for each access entry
+	private AtomicInteger cid;
+	protected ConcurrentHashMap<Integer, AccessEntry> log;
+	
+	public AccessLog () {
+		this.log = new ConcurrentHashMap<Integer,AccessEntry>();
+		cid = new AtomicInteger(0);
+	}
+
+	// TODO: add an access entry to the log for the provided message and return assigned id
+	public int add(String message) {
+		
+		int id = cid.getAndIncrement();
+		log.put(id, new AccessEntry(id, message));
+		
+		return id;
+	}
+		
+	// TODO: retrieve a specific access entry from the log
+	public AccessEntry get(int id) {
+		AccessEntry entry = null;
+		try {
+			entry = log.get(id);
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		return entry;
+		
+	}
+	
+	// TODO: clear the access entry log
+	public void clear() {
+		log.clear();
+		
+	}
+	
+	// TODO: return JSON representation of the access log
+	public String toJson () {
+    	
+		Gson gson = new Gson();
+    	
+    	return gson.toJson(log);
+    }
+}
